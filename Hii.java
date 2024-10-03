0dd498dbbecbfaa6ad1d2f751cb85b5ffeb7930a
Filzah2023 <azharuddinmails7013@gmail.com>
2024-10-03 12:39:47 +0530

    added  at end Hii.java

--- Hii.java
+++ Hii.java
@@ -1,6 +1,6 @@
 public class Hii {
     public static void main(String args []){
         System.out.println("hiii this is java,this is from main");
-        System.out.println("iam adding a new line THIS IS FOR CHECKING");
+        System.out.println("iam adding a new line THIS IS FOR CHECKING triggers");
     }
 }
